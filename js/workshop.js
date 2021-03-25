3c6a40385b26f4ed1140be3c84c2fb1f9c03b6d4
Spandan Pal <2spandan2@gmail.com>
2021-03-25 21:52:17 +0530

    added workshop - mobile

--- js/workshop.js
+++ js/workshop.js
@@ -11,7 +11,7 @@ var imgHeight = 250; // height of images (unit: px)
 var imgrad=25;
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicURL = null;
 var bgMusicControls = true; // Show UI music control
 
 /*
